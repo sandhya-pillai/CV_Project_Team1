df453ef67edab60343ecbb3451e8d79769226e1b
sandhya-pillai <pillai.sandhya25@gmail.com>
2023-06-24 09:57:27 -0400

    added previous and current frame in log

--- track.py
+++ track.py
@@ -174,7 +174,7 @@ def run(
                     s += f'{i}: '
                     global_key=txt_file_name = p.name
                     with open('output_print_log.txt' ,'a') as op:
-                        op.write('\n video name :'+global_key)
+                        op.write('\n video name :'+global_key + ' previous frame '+prev_frames[i]+' current frame '+im0)
                     save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
                 else:
                     p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
@@ -184,12 +184,12 @@ def run(
                         txt_file_name = p.stem
                         global_key= p.name
                         with open('output_print_log.txt' ,'a') as op:
-                            op.write('\n video name :'+global_key)
+                            op.write('\n video name :'+global_key + ' previous frame '+prev_frames[i]+' current frame '+im0)
                         save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
                     # folder with imgs
                     else:
                         with open('output_print_log.txt' ,'a') as op:
-                            op.write('\n video name :'+p.name)
+                            op.write('\n video name :'+p.name+ ' previous frame '+prev_frames[i]+' current frame '+im0)
                         global_key=txt_file_name = p.parent.name  # get folder name containing current img
                         save_path = str(save_dir / p.parent.name)  # im.jpg, vid.mp4, ...
                 curr_frames[i] = im0
